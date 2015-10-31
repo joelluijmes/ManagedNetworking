f42e585ed9fabb882af155d50b98c5b0f1476780
Joël Luijmes <joelluijmes@gmail.com>
2015-10-31 13:25:55 +0100

    Started working on udp..

--- NetworkingLibrary/Client.cs
+++ NetworkingLibrary/Client.cs
@@ -12,14 +12,26 @@ namespace NetworkingLibrary
     {
         protected readonly Socket _socket;
 
-        public Client()
+        public Client() : this(ProtocolType.Tcp) { }
+
+        public Client(ProtocolType type)
         {
-            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            if (type == ProtocolType.Udp)
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, type);
+            else if (type == ProtocolType.Tcp)
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, type);
+            else
+                throw new NotImplementedException("Unable to instantiate non tcp/udp client.");
         }
 
         internal Client(Socket socket)
         {
             _socket = socket;
+        }
+
+        public void Bind(EndPoint endPoint)
+        {
+            _socket.Bind(endPoint);
         }
 
         public bool Connect(EndPoint endPoint)
@@ -46,36 +58,74 @@ namespace NetworkingLibrary
         public int Send(byte[] buffer, int offset, int count)
             => _socket.Send(buffer, offset, count, SocketFlags.None);
 
-        public bool SendAll(byte[] buffer, int count)
-        {
-            var totalSent = 0;
+        public int SendTo(byte[] buffer, int offset, int count, EndPoint endPoint)
+            => _socket.SendTo(buffer, offset, count, SocketFlags.None, endPoint);
 
-            while (totalSent < count)
+        public bool SendAll(byte[] buffer, int count)
+            => TransceiveAll(Send, buffer, count);
+
+        public bool SendToAll(byte[] buffer, int count, EndPoint endPoint)
+        {
+            var total = 0;
+            while (total < count)
             {
-                var sent = Send(buffer, totalSent, count - totalSent);
-                if (sent == 0)
+                var current = _socket.SendTo(buffer, total, count - total, SocketFlags.None, endPoint);
+                if (current == 0)
                     return false;
 
-                totalSent += sent;
+                total += current;
             }
 
             return true;
         }
 
         public int Receive(byte[] buffer, int offset, int count)
-            => _socket.Receive(buffer, offset, count, SocketFlags.None);
-
-        public bool ReceiveAll(byte[] buffer, int count)
         {
-            var totalReceived = 0;
+            SocketError error;
+            var received = _socket.Receive(buffer, offset, count, SocketFlags.None, out error);
 
-            while (totalReceived < count)
+            // TODO: Add some more handling?
+            if (error != SocketError.Success)
+                throw new SocketException((int)error);
+
+            // recv returns 0 -> gracefully shutdown
+            if (received == 0)
+                _socket.Shutdown(SocketShutdown.Both);
+
+            return received;
+        }
+        
+        public bool ReceiveAll(byte[] buffer, int count)
+            => TransceiveAll(Receive, buffer, count);
+
+        public int ReceiveFrom(byte[] buffer, int offset, int count, ref EndPoint endPoint)
+            => _socket.ReceiveFrom(buffer, offset, count, SocketFlags.None, ref endPoint);
+
+        public bool ReceiveFromAll(byte[] buffer, int count, ref EndPoint endPoint)
+        {
+            var total = 0;
+            while (total < count)
             {
-                var received = Receive(buffer, totalReceived, count - totalReceived);
-                if (received == 0)
+                var current = _socket.ReceiveFrom(buffer, total, count - total, SocketFlags.None, ref endPoint);
+                if (current == 0)
                     return false;
 
-                totalReceived += received;
+                total += current;
+            }
+
+            return true;
+        }
+
+        private bool TransceiveAll(Func<byte[], int, int, int> func, byte[] buffer, int count)
+        {
+            var total = 0;
+            while (total < count)
+            {
+                var current = func(buffer, total, count - total);
+                if (current == 0)
+                    return false;
+
+                total += current;
             }
 
             return true;
