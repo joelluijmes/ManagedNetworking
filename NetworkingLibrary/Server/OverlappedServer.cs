ee2257d75e29b3b324297ab76fe14b79d3f5d47e
Joël Luijmes <joelluijmes@gmail.com>
2015-11-06 12:01:51 +0100

    Removed unused usings

--- NetworkingLibrary/Server/OverlappedServer.cs
+++ NetworkingLibrary/Server/OverlappedServer.cs
@@ -1,9 +1,5 @@
 ﻿using NetworkingLibrary.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetworkingLibrary.Server
 {
--- NetworkingLibrary/Server/Server.cs
+++ NetworkingLibrary/Server/Server.cs
@@ -1,14 +1,6 @@
 ﻿using NetworkingLibrary.Client;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetworkingLibrary.Server
 {
