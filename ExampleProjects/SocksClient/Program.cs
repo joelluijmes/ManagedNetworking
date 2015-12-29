e09e1f7ba5d9c23325ec66ba86dbbdabf628ad24
Joël Luijmes <Jol Luijmes>
2015-12-29 22:53:33 +0100

    Added socks exmaple project

---
+++ ExampleProjects/SocksClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkingLibrary.Socks.SOCKS5;
+
+namespace SocksClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2222);
+            var wikiPoint = new IPEndPoint(IPAddress.Parse("164.138.24.109"), 80);
+            var client = new Socks5Client(endPoint);
+            client.InitiateConnection(wikiPoint).Wait();
+
+            var request = "GET / HTTP/1.1\r\nHost: 164.138.24.109\r\n\r\n";
+            client.SendAll(Encoding.UTF8.GetBytes(request));
+
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+}
