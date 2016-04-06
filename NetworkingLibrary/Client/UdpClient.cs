5ef9ca8e1bcf90d48052f95b569c07e50c9c23cb
Joël Luijmes <joelluijmes@gmail.com>
2016-04-06 07:17:25 +0200

    Added *All functions

--- NetworkingLibrary/Client/UdpClient.cs
+++ NetworkingLibrary/Client/UdpClient.cs
@@ -5,8 +5,6 @@ namespace NetworkingLibrary.Client
 {
     public sealed partial class UdpClient : BaseClient
     {
-        private delegate int TransferFunc(byte[] buffer, int offset, int count);
-
         public UdpClient() : base(ProtocolType.Udp)
         { }
 
@@ -18,11 +16,43 @@ namespace NetworkingLibrary.Client
             ValidateTransferArguments(buffer, offset, ref count);
             return Socket.SendTo(buffer, offset, count, SocketFlags.None, remoteEndPoint);
         }
+
+        public bool SendToAll(byte[] buffer, int offset, int count, EndPoint remoteEndPoint)
+        {
+            ValidateTransferArguments(buffer, offset, ref count);
+            var transfered = 0;
+
+            while (transfered < count)
+            {
+                var current = Socket.SendTo(buffer, transfered, count - transfered, SocketFlags.None, remoteEndPoint);
+                if (current == 0)
+                    return false;
+                transfered += current;
+            }
+
+            return true;
+        }
         
         public int ReceiveFrom(byte[] buffer, int offset, int count, ref EndPoint remoteEndPoint)
         {
             ValidateTransferArguments(buffer, offset, ref count);
             return Socket.ReceiveFrom(buffer, offset, count, SocketFlags.None, ref remoteEndPoint);
+        }
+
+        public bool ReceiveFromAll(byte[] buffer, int count, ref EndPoint remoteEndPoint)
+        {
+            ValidateTransferAllArguments(buffer, ref count);
+            var transfered = 0;
+
+            while (transfered < count)
+            {
+                var current = Socket.ReceiveFrom(buffer, transfered, count - transfered, SocketFlags.None, ref remoteEndPoint);
+                if (current == 0)
+                    return false;
+                transfered += current;
+            }
+
+            return true;
         }
 
         public void Bind(EndPoint endPoint) 
