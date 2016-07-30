f55ce78d3d52bec5298accf05ae7d4afbd7fed0d
Joël Luijmes <joelluijmes@gmail.com>
2016-07-30 20:29:57 +0200

    removed ignored

--- ExampleProjects/Udp/Echor/Program.cs
+++ ExampleProjects/Udp/Echor/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using NetworkingLibrary.Client;
+
+using static System.Console;
 
 namespace Echor
 {
@@ -10,6 +9,23 @@ namespace Echor
     {
         static void Main(string[] args)
         {
+            var localEndPoint = new IPEndPoint(IPAddress.Any, 2222);
+
+            var client = new UdpClient();
+            client.Bind(localEndPoint);
+
+            WriteLine($"Bound on {localEndPoint}");
+
+            var buffer = new byte[2048];
+            while (true)
+            {
+                EndPoint endPoint = new IPEndPoint(0, 0);
+                var recv = client.ReceiveFrom(buffer, 0, buffer.Length, ref endPoint);
+                WriteLine($"Received {recv} bytes from: {(IPEndPoint)endPoint}");
+
+                var echoed = client.SendToAll(buffer, 0, recv, endPoint);
+                WriteLine($"Echoed: {echoed}");
+            }
         }
     }
 }
